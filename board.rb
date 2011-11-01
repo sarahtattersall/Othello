fc7b3d24280abfb32aaaadee451327c83770593a
Peter Hamilton <peter@inspiredpixel.net>
2011-11-01 13:54:01 +0000

    Some formatting changes

--- board.rb
+++ board.rb
@@ -5,15 +5,15 @@ class Board
     @size = size
     @board = Array.new(size) { Array.new(size, Cell.new)}
   end
-  
+
   def size
     return @size
   end
-  
+
   def get_cells
     return @board
   end
-  
+
   def place_piece(x, y, p)
     puts "Placing piece for player #{p} at #{x},#{y}"
     @board[x][y] = Cell.new(p)
@@ -27,10 +27,10 @@ class Board
           end
         end
       end 
-    #display_board
+      #display_board
     end
   end
-  
+
   def legal_move?(x, y, p)
     return false if @board[x][y].is_occupied?
     legal = false
@@ -44,7 +44,7 @@ class Board
     #[-1,0,1].each{ |i| [-1, 0, -1].each{ |j| legal |= find_piece(x+i, y+j, i, j, p)} }
     return legal
   end
-  
+
   def display_board
     puts "   0, 1, 2, 3, 4, 5, 6, 7"
     (0..(@size-1)).each do |i|
@@ -55,33 +55,33 @@ class Board
   def flip_pieces(x, y, x_offset, y_offset, p)
     flip = []
 
-	if (x >= 0 and y >= 0 and x < @size and y < @size)
-		if @board[x][y].is_occupied? and !@board[x][y].owned_by?(p)
-		  while( x >= 0 and y >= 0 and x < @size and y < @size )
-			return flip if @board[x][y].owned_by?(p)
-            flip << [x,y]
-			x += x_offset
-			y += y_offset
-		  end  
-		end
-	end
-	return []
+    if (x >= 0 and y >= 0 and x < @size and y < @size)
+      if @board[x][y].is_occupied? and !@board[x][y].owned_by?(p)
+        while( x >= 0 and y >= 0 and x < @size and y < @size )
+          return flip if @board[x][y].owned_by?(p)
+          flip << [x,y]
+          x += x_offset
+          y += y_offset
+        end  
+      end
+    end
+    return []
   end
-  
-  
+
+
 
   def find_piece(x, y, x_offset, y_offset, p)
     #puts "#{x}, #{y}, #{x_offset}, #{y_offset}"
-	if (x >= 0 and y >= 0 and x < @size and y < @size)
-		if @board[x][y].is_occupied? and !@board[x][y].owned_by?(p)
-		  while( x >= 0 and y >= 0 and x < @size and y < @size )
-			return true if @board[x][y].owned_by?(p)
-			x += x_offset
-			y += y_offset
-		  end  
-		end
-	end
-	return false
+    if (x >= 0 and y >= 0 and x < @size and y < @size)
+      if @board[x][y].is_occupied? and !@board[x][y].owned_by?(p)
+        while( x >= 0 and y >= 0 and x < @size and y < @size )
+          return true if @board[x][y].owned_by?(p)
+          x += x_offset
+          y += y_offset
+        end  
+      end
+    end
+    return false
   end
 
   def get_board
@@ -92,6 +92,6 @@ class Board
   def change_board( board )
     @board = board
   end
-    
+
 
 end
