a149f116e7945b0977b07b8e2ef79121846f7616
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-11-05 14:25:48 +0000

    Added basic ai move

--- ai_player.rb
+++ ai_player.rb
@@ -1,5 +1,12 @@
 require "./player"
 class AIPlayer < Player
 
+    def get_move(board)
+      for i in (0..(board.size-1))
+        for j in (0..(board.size-1))
+          return i, j if board.legal_move?(i, j, self)
+        end
+      end
+    end
 end
 
