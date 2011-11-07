8e7824f90e355defab572aa8917edaad23607c0f
Peter Hamilton <peter@inspiredpixel.net>
2011-11-07 09:03:10 +0000

    Refactores AIPlayer slightly

--- ai_player.rb
+++ ai_player.rb
@@ -1,7 +1,12 @@
 require "./player"
 class AIPlayer < Player
-
+  
     def get_move(board)
+      move_with_simple_intelligence(board)
+    end
+    
+    # Goes through every cell in sequence until it finds a playable position
+    def move_with_simple_intelligence(board)
       for i in (0..(board.size-1))
         for j in (0..(board.size-1))
            if board.legal_move?(i, j, self)
